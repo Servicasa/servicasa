edcb6fbf6b38515ef78b0e32b841a67cfb1a662e
mikhail usher <mikhail.usher@gmail.com>
2026-05-11 19:12:50 -0500

    Step 20: PWA - installable app with ServiCasa icon

---
+++ sw.js
@@ -0,0 +1,35 @@
+var CACHE = 'servicasa-v1';
+var ASSETS = [
+  '/servicasa/',
+  '/servicasa/index.html',
+  '/servicasa/manifest.json',
+  '/servicasa/icon-192.svg',
+  '/servicasa/icon-512.svg'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+});
